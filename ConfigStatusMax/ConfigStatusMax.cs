e468ca7d56264dda05a390d452771572f6216cce
kaz_mighty <kaz8032@gmail.com>
2023-04-24 19:30:25 +0900

    ConfigStatsMax now includes config for status rank required parameters, growth rate, and LOB points.

--- ConfigStatusMax/ConfigStatusMax.cs
+++ ConfigStatusMax/ConfigStatusMax.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Reflection;
 using System.Xml;
 using UnityEngine;
-using WorkerSpine;
 using Harmony;
 using AssemblyState;
 
@@ -21,15 +20,42 @@ namespace ConfigStatusMax
 				LoadConfig(directory + "/config.xml");
 
 				var harmony = HarmonyInstance.Create("kaz_mighty.ConfigStatusMax");
+
 				var targetClass = typeof(WorkerPrimaryStat);
-				var self = typeof(Harmony_Patch);
+				var patchClass = typeof(Harmony_Patch);
+				MethodInfo target;
+				HarmonyMethod prefix;
 				string[] methods = { "MaxStatR", "MaxStatW", "MaxStatB", "MaxStatP" };
 				foreach (string method in methods)
 				{
-					var target = targetClass.GetMethod(method);
-					var prefix = new HarmonyMethod(self.GetMethod(method));
+					target = targetClass.GetMethod(method);
+					prefix = new HarmonyMethod(patchClass.GetMethod(method));
 					harmony.Patch(target, prefix, null);
 				}
+
+				targetClass = typeof(UseSkill);
+				patchClass = typeof(StatLevel);
+				target = AccessTools.Method(targetClass, "CalculateLevelExp");
+				prefix = new HarmonyMethod(patchClass.GetMethod("CalculateLevelExp"));
+				harmony.Patch(target, prefix, null);
+
+				targetClass = typeof(AgentModel);
+				target = targetClass.GetMethod("CalculateStatLevel");
+				prefix = new HarmonyMethod(patchClass.GetMethod("CalculateStatLevel"));
+				harmony.Patch(target, prefix, null);
+				target = targetClass.GetMethod("CalculateStatLevelForCustomizing");
+				prefix = new HarmonyMethod(patchClass.GetMethod("CalculateStatLevelForCustomizing"));
+				harmony.Patch(target, prefix, null);
+
+				targetClass = typeof(Customizing.StatUI);
+				target = targetClass.GetProperty("MaxStatLevel").GetGetMethod();
+				prefix = new HarmonyMethod(patchClass.GetMethod("GetMaxStatLevel"));
+				harmony.Patch(target, prefix, null);
+
+				targetClass = typeof(Customizing.CustomizingWindow);
+				target = targetClass.GetMethod("SetAgentStatBonus");
+				prefix = new HarmonyMethod(patchClass.GetMethod("SetAgentStatBonus"));
+				harmony.Patch(target, prefix, null);
 			}
 			catch (Exception ex)
 			{
@@ -37,25 +63,25 @@ namespace ConfigStatusMax
 			}
 		}
 
-		static public bool MaxStatR(ref int __result)
+		public static bool MaxStatR(ref int __result)
 		{
 			__result = MaxStat("stat_max_r");
 			return false;
 		}
 
-		static public bool MaxStatW(ref int __result)
+		public static bool MaxStatW(ref int __result)
 		{
 			__result = MaxStat("stat_max_w");
 			return false;
 		}
 
-		static public bool MaxStatB(ref int __result)
+		public static bool MaxStatB(ref int __result)
 		{
 			__result = MaxStat("stat_max_b");
 			return false;
 		}
 
-		static public bool MaxStatP(ref int __result)
+		public static bool MaxStatP(ref int __result)
 		{
 			__result = MaxStat(null);
 			return false;
@@ -104,26 +130,27 @@ namespace ConfigStatusMax
 				var statLevelNode = xml.SelectSingleNode("config/statLevel");
 				if (statLevelNode != null)
 				{
-					rankDataList = new List<RankData>();
+					StatLevel.rankDataList = new List<RankData>();
 					var rankNodes = statLevelNode.SelectNodes("rank");
 					foreach(XmlNode rankNode in rankNodes)
 					{
 						var attributes = rankNode.Attributes;
 						var p1 = int.Parse(attributes.GetNamedItem("orMore").InnerText);
-						var p2 = double.Parse(attributes.GetNamedItem("growthRate").InnerText);
+						var p2 = float.Parse(attributes.GetNamedItem("growthRate").InnerText);
 
-						// attributes = rankNode.SelectSingleNode("upgrade").Attributes;
-						// var p3 = int.Parse(attributes.GetNamedItem("cost").InnerText);
-						// var p4 = int.Parse(attributes.GetNamedItem("min").InnerText);
-						// var p5 = int.Parse(attributes.GetNamedItem("max").InnerText);
+						attributes = rankNode.SelectSingleNode("upgrade").Attributes;
+						var p3 = int.Parse(attributes.GetNamedItem("cost").InnerText);
+						var p4 = int.Parse(attributes.GetNamedItem("min").InnerText);
+						var p5 = int.Parse(attributes.GetNamedItem("max").InnerText);
 
-						Harmony_Patch.rankDataList.Add(new RankData(p1, p2));
+						StatLevel.rankDataList.Add(new RankData(p1, p2, p3, p4, p5));
 					}
+					StatLevel.InitCost();
 				}
 				var maxLevelNode = xml.SelectSingleNode("config/upgradeMaxLevel");
 				if (maxLevelNode != null)
 				{
-					Harmony_Patch.upgradeMaxLevel = int.Parse(maxLevelNode.InnerText);
+					StatLevel.upgradeMaxLevel = int.Parse(maxLevelNode.InnerText);
 				}
 			}
 
@@ -136,39 +163,176 @@ namespace ConfigStatusMax
 				}
 				Debug.Log(log);
 
-				foreach (var rankdata in rankDataList)
+				foreach (var rankdata in StatLevel.rankDataList)
 				{
 					Debug.Log(rankdata.ToString());
 				}
-				Debug.Log(upgradeMaxLevel.ToString());
+				Debug.Log(StatLevel.upgradeMaxLevel.ToString());
 			}
 		}
 
 		static int[] maxStatValue = { 100, 120, 130, 150, 150 };
-		static List<RankData> rankDataList = null;
-		static int upgradeMaxLevel = 6;
 
 	}
 
 	public struct RankData
 	{
 		public readonly int orMore;
-		public readonly double growthRate;
+		public readonly float growthRate;
 		public readonly int upgradeCost;
 		public readonly int upgradeMin;
 		public readonly int upgradeMax;
-		public RankData(int p1, double p2)
+		public RankData(int p1, float p2, int p3, int p4, int p5)
 		{
 			orMore = p1;
 			growthRate = p2;
-			upgradeCost = 0;
-			upgradeMin = 0;
-			upgradeMax = 0;
+			upgradeCost = p3;
+			upgradeMin = p4;
+			upgradeMax = p5;
 		}
 		public new string ToString()
 		{
 			return String.Format("{0} {1} {2} {3} {4}", orMore, growthRate, upgradeCost, upgradeMin, upgradeMax);
 		}
+	}
+
+	public static class StatLevel
+	{
+		public static List<RankData> rankDataList = null;
+		public static int upgradeMaxLevel = 6;
+		public static float[] diffGrowthRate = { 1.4f, 1.2f, 1f, 1f, 0.8f, 0.6f, 0.4f, 0.2f };
+
+		
+		public static void InitCost()
+		{
+			var costN = new int[rankDataList.Count];
+			var costP = new int[rankDataList.Count];
+			for (var i = 0; i < rankDataList.Count; i++)
+			{
+				costN[i] = rankDataList[i].upgradeCost;
+				costP[i] = rankDataList[i].upgradeCost * 3;
+			}
+			Customizing.StatUI.RCost = costN;
+			Customizing.StatUI.WCost = costN;
+			Customizing.StatUI.BCost = costN;
+			Customizing.StatUI.PCost = costP;
+		}
+
+
+		public static bool CalculateStatLevel(ref int __result, int stat)
+		{
+			var i = Math.Min(5, rankDataList.Count) - 1;
+			__result = 1;
+			for (; i >= 0; i--)
+			{
+				if (stat >= rankDataList[i].orMore)
+				{
+					__result = i + 1;
+					break;
+				}
+			}
+			return false;
+		}
+
+		public static bool CalculateStatLevelForCustomizing(ref int __result, int stat)
+		{
+			__result = 1;
+			for (var i = rankDataList.Count - 1; i >= 0; i--)
+			{
+				if (stat >= rankDataList[i].orMore)
+				{
+					__result = i + 1;
+					break;
+				}
+			}
+			Debug.Log(String.Format("CalculateStatLevelforCustomizing: {0}", __result));
+			return false;
+		}
+
+		public static bool CalculateLevelExp(UseSkill __instance, ref float __result, RwbpType rwbpType)
+		{
+			int statLevelEx = 0;
+			WorkerPrimaryStat addedStat = __instance.agent.primaryStat.GetAddedStat(__instance.agent.primaryStatExp);
+			float expMulti = 1f;
+			switch (rwbpType)
+			{
+				case RwbpType.R:
+					statLevelEx = AgentModel.CalculateStatLevelForCustomizing(addedStat.hp);
+					break;
+				case RwbpType.W:
+					statLevelEx = AgentModel.CalculateStatLevelForCustomizing(addedStat.mental);
+					break;
+				case RwbpType.B:
+					statLevelEx = AgentModel.CalculateStatLevelForCustomizing(addedStat.work);
+					break;
+				case RwbpType.P:
+					statLevelEx = AgentModel.CalculateStatLevelForCustomizing(addedStat.battle);
+					break;
+			}
+			int statLevel = Math.Min(statLevelEx, 5);
+			int levelDiff = statLevel - __instance.targetCreature.GetRiskLevel() + 3;
+			if (levelDiff >= 0 && levelDiff < diffGrowthRate.Length)
+			{
+				expMulti = diffGrowthRate[levelDiff];
+			}
+
+			var growthRate = 0.0f;
+			if (statLevelEx <= 0 || statLevelEx > rankDataList.Count)
+			{
+				growthRate = rankDataList[rankDataList.Count - 1].growthRate;
+				Debug.LogError("statLevelEx is Out of Range!");
+			}
+			else
+			{
+				growthRate = rankDataList[statLevelEx - 1].growthRate;
+			}
+			expMulti *= growthRate;
+			if (rwbpType == RwbpType.P)
+			{
+				expMulti /= 3f;
+			}
+			__result = expMulti;
+			Debug.Log(String.Format("statLevel: {0}", statLevel));
+			Debug.Log(String.Format("statLevelEx: {0}", statLevelEx));
+			Debug.Log(String.Format("growthRate: {0}", growthRate));
+			Debug.Log(String.Format("expMulti: {0}", __result));
+			return false;
+		}
+
+		public static bool GetMaxStatLevel(ref int __result)
+		{
+			__result = (!MissionManager.instance.ExistsFinishedBossMission(SefiraEnum.CHOKHMAH)) ? 5 : upgradeMaxLevel;
+			return false;
+		}
+
+		public static bool SetAgentStatBonus(AgentModel agent, Customizing.AgentData data)
+		{
+			int level = agent.level;
+			agent.primaryStat.hp = SetRandomStatValue(agent.primaryStat.hp, data.RLevel, data.statBonus.rBonus);
+			agent.primaryStat.mental = SetRandomStatValue(agent.primaryStat.mental, data.WLevel, data.statBonus.wBonus);
+			agent.primaryStat.work = SetRandomStatValue(agent.primaryStat.work, data.BLevel, data.statBonus.bBonus);
+			agent.primaryStat.battle = SetRandomStatValue(agent.primaryStat.battle, data.PLevel, data.statBonus.pBonus);
+			agent.UpdateTitle(level);
+			return false;
+		}
+
+		public static int SetRandomStatValue(int original, int currentLevel, int bonusLevel)
+		{
+			if (bonusLevel == 0)
+			{
+				return original;
+			}
+			int totalLevel = currentLevel + bonusLevel;
+			if (totalLevel <= 0 || totalLevel > rankDataList.Count)
+			{
+				Debug.LogError(string.Format("SetRandomStatValue: totalLevel {0} is Out of Range!", totalLevel));
+				return original;
+			}
+			int min = rankDataList[totalLevel - 1].upgradeMin;
+			int max = rankDataList[totalLevel - 1].upgradeMax;
+			return UnityEngine.Random.Range(min, max);
+		}
+
 	}
 
 }
